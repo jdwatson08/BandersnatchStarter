2a68dc8c3b408d230707f9b0270c8a43a2ff9b88
jdwatson08 <jdwatson31@gmail.com>
2023-09-11 20:15:05 -0400

    Sprint 1 refactor

--- app/data.py
+++ app/data.py
@@ -18,9 +18,8 @@ class Database:
 
     def seed(self, count=1000):
         """Fills the mongodb database with 1000 random instances of the monster class"""
-        for _ in range(count):
-            doc = Monster().to_dict()
-            self.collection.insert_one(doc)
+        doc = [Monster().to_dict() for _ in range(count)]
+        self.collection.insert_many(doc)
 
     def reset(self):
         """Deletes every instance of the Monster class from the mongodb database"""
@@ -32,15 +31,11 @@ class Database:
 
     def dataframe(self) -> DataFrame:
         """Takes oll of the entries in the mongodb database and stores them in database format. Returns the dataframe"""
-        cursor = self.collection.find()
-        list_cur = list(cursor)
-        df = DataFrame(list_cur)
+        df = DataFrame(self.collection.find())
         return df
 
     def html_table(self) -> str:
         """Takes oll of the entries in the mongodb database and stores them in html format. Returns the html table"""
-        cursor = self.collection.find()
-        list_cur = list(cursor)
-        df = DataFrame(list_cur)
+        df = self.dataframe()
         del df[df.columns[0]]
         return df.to_html()
