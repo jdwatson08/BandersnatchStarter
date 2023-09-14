63544b58c1dcaeacb9e7cee74eefa3fb0c158ff3
jdwatson08 <jdwatson31@gmail.com>
2023-09-14 13:49:16 -0400

    Sprint 2 two graph idea

--- app/graph.py
+++ app/graph.py
@@ -4,18 +4,23 @@ from pandas import DataFrame
 
 def chart(df: DataFrame, x: str, y: str, target: str) -> Chart:
     del df[df.columns[0]]
-    graph = Chart(
+    graph1 = Chart(
         df,
-        title=f"{y} by {x} for {target}", height=400, width=400, padding=20
-    ).mark_circle(size=100).encode(x=x,
-                                   y=y,
-                                   color=target,
-                                   tooltip=Tooltip(df.columns.to_list())
-                                   )
+        title=f"{y} by {x} for {target}", height=400, width=300
+    ).mark_point(size=100).encode(x=x,
+                                  y=y,
+                                  color=target,
+                                  tooltip=Tooltip(df.columns.to_list())
+                                  )
     themes.enable('dark')
-    graph = graph.configure_title(fontSize=24)
-    return graph
-    # graph = Chart(df, title=f"{y} by {x} for {target}").mark_point().encode(
-    #
-    # )
-    # return graph
+
+    graph2 = Chart(
+        df,
+        title=f"{y} by {x} for {target}", height=400, width=300
+    ).mark_bar(size=100).encode(x=x,
+                                y=y,
+                                tooltip=Tooltip(df.columns.to_list())
+                                )
+    themes.enable('dark')
+    con = graph1 | graph2
+    return con
